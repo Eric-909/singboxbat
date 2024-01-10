ab4151189f8688e4ab138f3a1a8f328a2cd5c7c4
mianyang <i@mareep.net>
2024-01-10 17:46:44 +0800

    INIT

---
+++ start.bat
@@ -0,0 +1,47 @@
+@echo off
+setlocal enabledelayedexpansion
+chcp 1252 > nul
+
+cd /d "%~dp0"
+
+set "subFolder=sub"
+set "counter=0"
+set "defaultChoice=1" 
+
+color 0A
+echo.
+echo Please Choose Config Profile:
+echo.
+
+for %%F in (%subFolder%\*) do (
+    set /a counter+=1
+    echo !counter!. %%~nxF
+    set "file[!counter!]=%%~dpnxF"
+)
+echo.
+set /p "choice=Enter Your Choice [Default:!defaultChoice!]: "
+if "!choice!"=="" set "choice=!defaultChoice!"
+for /f %%A in ("!choice!") do set "choice=%%A"
+set "choice=!choice: =!"
+
+if !choice! geq 1 if !choice! leq !counter! (
+    set "selectedFile=!file[%choice%]!"
+    echo selected file: !selectedFile!
+
+set "checkResult="
+call set "checkCommand=sing-box.exe check -c "!selectedFile!" 2>&1"
+for /f "delims=" %%i in ('!checkCommand!') do set "checkResult=%%i"
+
+if not "!checkResult!"=="" (
+    echo "check failed"
+    echo !checkResult!
+    pause
+) else (
+    start "singbox" "sing-box.exe" run -c "!selectedFile!"
+)
+    
+) else (
+    echo error...
+    pause
+    exit /b 1
+)
---
+++ update.bat
@@ -0,0 +1,36 @@
+@echo off
+setlocal enabledelayedexpansion
+
+
+set "scriptDir=%~dp0"
+
+set "subFolder=%scriptDir%sub"
+set "wgetPath=%scriptDir%wget.exe"
+set "urlFile=%scriptDir%SUB.txt"
+
+set "timeoutSeconds=10"
+
+if not exist "!subFolder!" mkdir "!subFolder!"
+
+for /f "tokens=1,* delims==" %%a in (!urlFile!) do (
+    if "%%a"=="NAME" (
+        set "fileName=%%b"
+    ) else if "%%a"=="URL" (
+        set "url=%%b"
+
+        if not "!url!"=="" (
+            !wgetPath! -O !subFolder!\!fileName! -T !timeoutSeconds! -t 1 !url!
+            
+            if errorlevel 1 (
+                echo "CONFIG !fileName! DOWNLOAD FAILED."
+            ) else (
+                echo "CONFIG !fileName! DOWNLOAD SUCCESSED!"
+            )
+        ) else (
+            echo "CONFIG !fileName! IS EMPTY."
+        )
+    )
+)
+
+echo "BYE"
+pause
